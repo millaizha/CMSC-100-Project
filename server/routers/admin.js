6ff3dea4aaac95d85d81b000a3dbd35a78bc1234
FarrelBeso <vabeso@up.edu.ph>
2024-05-18 13:52:12 +0800

    docs: admin routes documentation; update on auth routes docs

--- server/routers/admin.js
+++ server/routers/admin.js
@@ -8,7 +8,51 @@ import { verifyToken, verifyIfAdmin } from "../utils/middleware.js";
 
 const adminRoutes = express.Router();
 
+/**
+ * POST /admin/addProduct
+ * Add one product.
+ *
+ * Requires the Authorization header with the value "Bearer <token>".
+ * User accessing it must be an admin.
+ *
+ * Inputs for req.body:
+ * productId - String (unique)
+ * name - String (unique)
+ * description - String (optional)
+ * price - Number
+ * type - Number (1 or 2)
+ * quantity - Number
+ *
+ * Response:
+ * If successful: Status code 201; "Product created successfully"
+ * Else: Status code 500; "Error adding the new product"
+ */
 adminRoutes.post("/addProduct", verifyToken, verifyIfAdmin, addProduct);
+
+/**
+ * GET /admin/getProductListings
+ * Get all products in the database.
+ *
+ * Requires the Authorization header with the value "Bearer <token>".
+ * User accessing it must be an admin.
+ *
+ * Inputs for req.body:
+ * sortOption - Object
+ *
+ * Example of the req.body:
+ *
+ * {
+ *  "sortOption": {<key>: <asc or desc>}
+ * }
+ *
+ * <key> can be "name", "type", "price", or "quantity"
+ * <asc or desc>: 1 for ascending, -1 for descending
+ * Only one key is allowed for now.
+ *
+ * Response:
+ * If successful: Status code 200, <list of products>
+ * Else: Status code 500; "Unable to get products"
+ */
 adminRoutes.get(
   "/getProductListings",
   verifyToken,
@@ -16,8 +60,22 @@ adminRoutes.get(
   getProductListings
 );
 
+/**
+ * GET /admin/registeredUsers
+ * Get all registered users in the database.
+ *
+ * Requires the Authorization header with the value "Bearer <token>".
+ * User accessing it must be an admin.
+ *
+ * Inputs for req.body:
+ * None
+ *
+ * Response:
+ * If successful: Status code 200, <list of users>
+ * Else: Status code 500; "Unable to get users"
+ */
 adminRoutes.get(
-  "/getRegisteredUsers",
+  "/registeredUsers",
   verifyToken,
   verifyIfAdmin,
   getRegisteredUsers
