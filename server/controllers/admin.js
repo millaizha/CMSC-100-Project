92e868ed1940e54c50117e1f2009342749ea9152
FarrelBeso <vabeso@up.edu.ph>
2024-05-18 09:08:35 +0800

    feat: init adding the new product

---
+++ server/controllers/admin.js
@@ -0,0 +1,28 @@
+import Product from "../models/productModel";
+
+const addProduct = async (req, res) => {
+  try {
+    const {
+      productId,
+      name,
+      description = null,
+      price,
+      type,
+      quantity,
+    } = req.body;
+    const newProduct = new Product({
+      productId,
+      name,
+      description,
+      price,
+      type,
+      quantity,
+    });
+    await newProduct.save();
+    res.status(201).json({ message: "Product created successfully." });
+  } catch (error) {
+    res.status(500).json({ error: "Error adding the new product." });
+  }
+};
+
+export { addProduct };
