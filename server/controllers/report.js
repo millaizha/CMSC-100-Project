5fb277c74d4278a25ab98c030f31916b39090969
FarrelBeso <vabeso@up.edu.ph>
2024-05-24 23:46:07 +0800

    feat: added weekly, monthly, and yearly reports

--- server/controllers/report.js
+++ server/controllers/report.js
@@ -73,6 +73,7 @@ const getProductsSold = async (req, res) => {
 };
 
 // it just returns the total sales per week provided
+// the _id denotes the _id-th week in a year
 const getWeeklyReport = async (req, res) => {
   const { earliestDate, latestDate } = req.body;
   try {
@@ -89,31 +90,8 @@ const getWeeklyReport = async (req, res) => {
       },
       {
         $group: {
-          _id: "$productId",
-          totalQuantity: { $sum: "$quantity" },
-        },
-      },
-      {
-        $lookup: {
-          from: "products",
-          localField: "_id",
-          foreignField: "_id",
-          as: "productInfo", // New alias
-        },
-      },
-      {
-        $unwind: "$productInfo",
-      },
-      {
-        $project: {
-          name: "$productInfo.name",
-          description: "$productInfo.description",
-          price: "$productInfo.price",
-          type: "$productInfo.type",
-          totalQuantity: "$totalQuantity",
-          totalSales: {
-            $multiply: ["$totalQuantity", "$productInfo.price"],
-          },
+          _id: { $week: "$dateTimeOrdered" },
+          aggregateCost: { $sum: "$totalCost" },
         },
       },
     ]);
@@ -124,4 +102,70 @@ const getWeeklyReport = async (req, res) => {
   }
 };
 
-export { getRecentSales, getProductsSold };
+// it just returns the total sales per month provided
+// the _id denotes the numerical month in a year
+const getMonthlyReport = async (req, res) => {
+  const { earliestDate, latestDate } = req.body;
+  try {
+    const productsSold = await Transaction.aggregate([
+      {
+        // only completed transactions
+        $match: {
+          status: 1,
+          dateTimeOrdered: {
+            $gte: new Date(earliestDate),
+            $lte: new Date(latestDate),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$dateTimeOrdered" },
+          aggregateCost: { $sum: "$totalCost" },
+        },
+      },
+    ]);
+
+    res.status(200).json(productsSold);
+  } catch (error) {
+    res.status(500).json({ error: "Unable to get report." });
+  }
+};
+
+// it just returns the total sales per month provided
+// the _id denotes the year
+const getYearlyReport = async (req, res) => {
+  const { earliestDate, latestDate } = req.body;
+  try {
+    const productsSold = await Transaction.aggregate([
+      {
+        // only completed transactions
+        $match: {
+          status: 1,
+          dateTimeOrdered: {
+            $gte: new Date(earliestDate),
+            $lte: new Date(latestDate),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $year: "$dateTimeOrdered" },
+          aggregateCost: { $sum: "$totalCost" },
+        },
+      },
+    ]);
+
+    res.status(200).json(productsSold);
+  } catch (error) {
+    res.status(500).json({ error: "Unable to get report." });
+  }
+};
+
+export {
+  getRecentSales,
+  getProductsSold,
+  getWeeklyReport,
+  getMonthlyReport,
+  getYearlyReport,
+};
--- server/routers/report.js
+++ server/routers/report.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { getProductsSold, getRecentSales } from "../controllers/report.js";
+import {
+  getMonthlyReport,
+  getProductsSold,
+  getRecentSales,
+  getWeeklyReport,
+  getYearlyReport,
+} from "../controllers/report.js";
 import { verifyIfAdmin, verifyToken } from "../utils/middleware.js";
 
 const reportRoutes = express.Router();
@@ -11,6 +17,27 @@ reportRoutes.get(
   verifyToken,
   verifyIfAdmin,
   getProductsSold
+);
+
+reportRoutes.get(
+  "/getWeeklyReport",
+  verifyToken,
+  verifyIfAdmin,
+  getWeeklyReport
+);
+
+reportRoutes.get(
+  "/getMonthlyReport",
+  verifyToken,
+  verifyIfAdmin,
+  getMonthlyReport
+);
+
+reportRoutes.get(
+  "/getYearlyReport",
+  verifyToken,
+  verifyIfAdmin,
+  getYearlyReport
 );
 
 export default reportRoutes;
