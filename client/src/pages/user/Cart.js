b54377b779b5113f8af43d43f074c5bd96e71377
kyleBSCS <kpvinuya@up.edu.ph>
2024-05-25 18:32:03 +0800

    feat: incorporated shipping fee with offer

--- client/src/pages/user/Cart.js
+++ client/src/pages/user/Cart.js
@@ -1,7 +1,7 @@
 import CartListCard from "../../components/CartListCard";
 import Navbar from "../../components/Navbar";
 import { FaTrash } from "react-icons/fa";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import IMAGE from "../../assets/shop/empty.png";
 import { useNavigate } from "react-router-dom";
@@ -10,13 +10,21 @@ export default function Cart({ cartList }) {
   const { cart } = useContext(CartContext);
   const navigate = useNavigate();
 
-  let totalPrice = 0;
-  for (const item of cart) {
-    if (item.selectedQuantity > 0) {
-      console.log(item.price * item.selectedQuantity) 
-      totalPrice = totalPrice + (item.price * item.selectedQuantity);
+  const [totalPrice, setTotalPrice] = useState(0); 
+
+  useEffect(() => {
+    let newTotalPrice = 0;
+    for (const item of cart) {
+      if (item.selectedQuantity > 0) {
+        newTotalPrice += item.price * item.selectedQuantity;
+      }
     }
-  }
+    setTotalPrice(newTotalPrice);
+  }, [cart]);
+
+
+  const shippingFee = totalPrice > 500 ? 0 : 50;
+
   return (
     <div className="h-screen w-screen">
       <Navbar />
@@ -37,18 +45,30 @@ export default function Cart({ cartList }) {
                 <CartListCard product={item} />
               ))}
 
-              <div className="self-end mt-4 flex flex-col items-end gap-6">
+              <div className="self-end mt-4 flex flex-col items-end gap-4">
+                <div className="flex items-end gap-1">
+                  <h1 className="text-xl font-bold mr-12">SHIPPING FEE</h1>
+                  {totalPrice <= 500 ? 
+                  (<><div className="text-xl font-bold">PHP</div>
+                  <div className="text-4xl font-bold">{shippingFee}</div></>) :
+                   (<div className="text-xl font-bold">FREE</div>)
+                  }
+
+                  <div className="spacer w-[90px]"></div>
+                </div>
                 <div className="flex items-end gap-1">
                   <h1 className="text-xl font-bold mr-12">TOTAL</h1>
 
                   <div className="text-xl font-bold">PHP</div>
-                  <div className="text-4xl font-bold">{totalPrice.toFixed(2)}</div>
+                  <div className="text-4xl font-bold">{(totalPrice + shippingFee).toFixed(2)}</div>
 
                   <div className="spacer w-[90px]"></div>
                 </div>
 
-                <button className="form-button">Confirm Order</button>
+                <button className="form-button mt-3">Confirm Order</button>
               </div>
+
+              <span className="text-gray-600">* Orders with a total above 500 PHP will get FREE delivery!</span>
             </div>
           )}
         </div>
